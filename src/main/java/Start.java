9db4fb61454bdb7ce9491607533d4187ae16bb84
Anna Pietrzak <195680@edu.p.lodz.pl>
2017-10-12 18:32:00 +0200

    zalazek zadania

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,9 +1,72 @@
-/**
- * @author Yuriy on 09.10.2017.
- */
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
 public class Start {
-    public static void main(String[] args) {
-        System.out.println("Hi!");
+    public static void main(String[] args)
+    {
+        final int numberOfElements = 10;
+        final int numberOfProcesses = 3;
+        final int numberOfPermits = 1;
+
+        Semaphore semaphore = new Semaphore(numberOfPermits, true);
+        MyThread p[] = new MyThread[numberOfProcesses];
+
+        int A[] = new int[numberOfElements];
+        int B[] = new int[numberOfElements];
+        Random random = new Random();
+        for(int i=0; i<A.length; i++){
+            A[i] = random.nextInt(10) + 1;
+            B[i] = random.nextInt(10) + 1;
+            System.out.println(A[i] + " " + B[i]);
+        }
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+        System.out.println("hahahahahahha");
+        for(int i=0; i<A.length; i++){
+            System.out.println(A[i] + " " + B[i]);
+        }
     }
 }
+
+class MyThread extends Thread
+{
+    private int threadId;
+    private Semaphore semaphore;
+
+    public MyThread(Semaphore semaphore) {
+        this.semaphore = semaphore;
+    }
+
+    public void setThreadId(int threadId) {
+        this.threadId = threadId;
+    }
+
+    public void run()
+    {
+        for (int i = 0; i < 3; i++) {
+            try {
+                semaphore.acquire();
+                System.out.println(threadId+ " " + 1);
+                semaphore.release();
+            } catch (InterruptedException e) {
+                System.out.println("Exception " + e.toString());
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
